717eff5380938fa823d16762fde92c01b5c7639a
dimitrissthe <dimitrissthe@gmail.com>
2026-05-20 19:22:08 +0300

    Delete Project directory

--- Project/constraints/prescription_constraint.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE prescription
-ADD CONSTRAINT unique_prescription_combo 
-UNIQUE (doctor_id, patient_id, drug_id, start_date);
--- Project/constraints/shift_constraint.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE shift
-ADD CONSTRAINT unique_edpartment_date_shift
-UNIQUE (department_id, shift_date, shift_type);
